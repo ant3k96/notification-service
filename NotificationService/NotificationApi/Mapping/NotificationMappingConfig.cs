90b36aa8832d2d44e043805b66f47039087445d7
Antoni Kistowski <anki@netcompany.com>
2024-08-07 23:47:51 +0200

    feat: Adding mapping config

---
+++ NotificationService/NotificationApi/Mapping/NotificationMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using Mapster;
+using Notification.Api.Model;
+using Notification.Domain.Notifications;
+
+namespace Notification.Api.Mapping
+{
+    public class NotificationMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<SendMessageRequest, NotificationMessage>()
+                .Map(dest => dest.Sender, src => src.From)
+                .Map(dest => dest.Receiver, src => src.To);
+        }
+    }
+}
